c91e487e6ad54fcf2d3a60e7c968a63dc382db94
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2024-08-22 21:47:44 +0900

    Add u i p s ailias (idk if it works)

--- .zshrc.d/99-alias.zsh
+++ .zshrc.d/99-alias.zsh
@@ -2,3 +2,21 @@ alias reload='exec zsh'
 alias ls='ls --color=auto'
 alias la='ls -lah'
 alias ff='fastfetch'
+
+sudo=sudo
+if type doas > /dev/null; then
+  sudo=doas
+fi
+
+if [[ -f /etc/os-release ]]; then
+  case "$(grep ^ID= /etc/os-release | cut -d= -f2)" in
+    debian)
+      alias u='sudo apt update && sudo apt upgrade -y'
+      alias i='sudo apt install -y'
+      alias p='sudo apt purge --autoremove -y'
+      alias s='apt search'
+      ;;
+    *)
+      ;;
+  esac
+fi

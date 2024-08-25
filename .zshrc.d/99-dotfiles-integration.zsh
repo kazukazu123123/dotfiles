ac98bae656094ef894df548de7adf1cd5bff353c
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2024-08-25 18:30:26 +0900

    A

--- .zshrc.d/99-dotfiles-integration.zsh
+++ .zshrc.d/99-dotfiles-integration.zsh
@@ -1,2 +1,2 @@
 alias cdf="cd ~/.dotfiles"
-alias dfu="zsh -c 'cd ~/.dotfiles && git stash -a && git switch main && git pull --ff-only && git switch - && git stash pop' && zinit update --quiet && exec zsh"
+alias dfu="zsh -c 'cd ~/.dotfiles && git stash -a && git switch main && git pull --ff-only && git switch - && git stash pop' && zimfw uninstall -q && zinfw install && zimfw update && exec zsh"

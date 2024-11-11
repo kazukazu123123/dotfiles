ac357074dfc24525c1bb07f8c60f244e9e5bd590
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2024-11-11 22:59:11 +0900

    Remove nala

--- .zshrc.d/99-alias.zsh
+++ .zshrc.d/99-alias.zsh
@@ -11,15 +11,8 @@ if type doas > /dev/null; then
 fi
 
 if [[ -f /etc/os-release ]] && grep -q debian /etc/os-release; then
-    if type nala > /dev/null; then
-        alias u="${sudo} nala update && ${sudo} nala upgrade -y"
-        alias i="${sudo} nala install -y"
-        alias p="${sudo} nala purge --autoremove -y"
-        alias s="nala search"
-    else
-        alias u="${sudo} apt update && ${sudo} apt upgrade -y"
-        alias i="${sudo} apt install -y"
-        alias p="${sudo} apt purge --autoremove -y"
-        alias s="apt search"
-    fi
+    alias u="${sudo} apt update && ${sudo} apt upgrade -y"
+    alias i="${sudo} apt install -y"
+    alias p="${sudo} apt purge --autoremove -y"
+    alias s="apt search"
 fi

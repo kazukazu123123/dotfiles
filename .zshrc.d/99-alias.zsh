caf73b596bbf700fb404e13f8a5c01207c742f1d
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2024-11-14 13:49:04 +0900

    Add 'cargo' and 'git' short alias

--- .zshrc.d/99-alias.zsh
+++ .zshrc.d/99-alias.zsh
@@ -5,14 +5,24 @@ alias ff='fastfetch'
 
 alias cls='clear'
 
+alias c='cargo'
+alias g='git'
+
 sudo=sudo
 if type doas > /dev/null; then
     sudo=doas
 fi
 
-if [[ -f /etc/os-release ]] && grep -q debian /etc/os-release; then
-    alias u="${sudo} apt update && ${sudo} apt upgrade -y"
-    alias i="${sudo} apt install -y"
-    alias p="${sudo} apt purge --autoremove -y"
-    alias s="apt search"
+if [[ -f /etc/os-release ]]; then
+    if grep -q debian /etc/os-release; then
+        alias u="${sudo} apt update && ${sudo} apt upgrade -y"
+        alias i="${sudo} apt install -y"
+        alias p="${sudo} apt purge --autoremove -y"
+        alias s="apt search"
+    elif grep -q void /etc/os-release; then
+        alias u="${sudo} xbps-install -Syu"
+        alias i="${sudo} xbps-install -y"
+        alias p="${sudo} xbps-remove -Ry"
+        alias s="xbps-query -Rs"
+    fi
 fi
